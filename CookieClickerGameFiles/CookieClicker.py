80202844aa18915d11d01ebf20b5a26e4321e9ea
CodeMaster7000 <CodeMaster7K@gmail.com>
2024-07-04 23:30:07 +0100

    Upload CookieClicker.py

---
+++ CookieClickerGameFiles/CookieClicker.py
@@ -0,0 +1,30 @@
+import turtle
+
+wn = turtle.Screen()
+
+wn.title("CodeMaster7000's Cookie Clicker")
+wn.bgcolor("black")
+wn.register_shape("Cookie.gif")
+
+cookie = turtle.Turtle()
+cookie.shape("Cookie.gif")
+cookie.speed(0)
+
+clicks = 0
+
+pen = turtle.Turtle()
+pen.hideturtle()
+pen.color("white")
+pen.penup()
+pen.goto(0, 400)
+pen.write(f"Clicks: {clicks}", align="center", font=("Calibri", 28, "normal")) # Customize for your games
+
+def clicked(x, y):
+    global clicks
+    clicks += 1
+    pen.clear()
+    pen.write(f"Clicks: {clicks}", align="center", font=("Calibri", 28, "normal")) # Customize for your games
+    
+cookie.onclick(clicked)
+
+wn.mainloop()
